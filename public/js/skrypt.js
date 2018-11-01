ac4e62f8d2912ef24623957a8acd8ecc85b1667d
mmazepa <maksymilianmariusz@gmail.com>
2018-11-01 22:40:02 +0100

    Rozpoczęcie prac nad szatą graficzną i skryptową obsługą interfejsu użytkownika.

--- public/js/skrypt.js
+++ public/js/skrypt.js
@@ -4,6 +4,39 @@ document.onreadystatechange = function()
 {
     if(document.readyState === "interactive")
     {
-        // ...
+        $(function()
+        {
+            function selectMenuOption()
+            {
+                let currentLocation = window.location.href.split("/").pop();
+                switch (currentLocation)
+                {
+                    case "":
+                        document.getElementById("homepage").classList.add("activeMenuOption");
+                        break;
+                    case "signup":
+                        document.getElementById("signup").classList.add("activeMenuOption");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            selectMenuOption();
+
+            $(".menuElement").on("click", function()
+            {
+                switch (this.id)
+                {
+                    case "homepage":
+                        location.replace("/");
+                        break;
+                    case "signup":
+                        location.replace("/signup");
+                        break;
+                    default:
+                        break;
+                }
+            });
+        });
     }
 };
