97f822b9fe676b17c3cf992194d94157443d7315
MPilkou <mpilkou@gmail.com>
2018-11-21 17:11:59 +0100

    edit db script

--- db_scripts/sql_script.sql
+++ db_scripts/sql_script.sql
@@ -1,8 +1,16 @@
--- create db
---DROP DATABASE IF EXISTS mfip;
---CREATE DATABASE mfip;
+-- revoke priv if exist fun
+DO $$DECLARE count int;
+BEGIN
+SELECT count(*) INTO count FROM pg_roles WHERE rolname = 'mfip_admin';
+IF count > 0 THEN
+    EXECUTE 
+        '
+                REVOKE ALL ON DATABASE mfip FROM mfip_admin;
+        ';
+END IF;
+END$$;
 -- create admin
--- DROP ROLE IF EXISTS "admin";
+DROP ROLE IF EXISTS "mfip_admin";
 
 
 
@@ -12,60 +20,42 @@ GRANT ALL ON DATABASE mfip TO mfip_admin;
 
 \connect mfip;
 
-
-DROP INDEX IF EXISTS "Work_History_id_company";
-DROP INDEX IF EXISTS "Workstation_id_company";
-DROP INDEX IF EXISTS "List_Skills_id_list_skills";
-
-DROP TABLE IF EXISTS "List_Skills";
-DROP TABLE IF EXISTS "Skill";
-DROP TABLE IF EXISTS "Company";
-DROP TABLE IF EXISTS "Workstation";
-DROP TABLE IF EXISTS "Employee";
-DROP TABLE IF EXISTS "Group_History";
-DROP TABLE IF EXISTS "Work_History";
-DROP TABLE IF EXISTS "Vacation_History";
-DROP TABLE IF EXISTS "Adres";
-
-CREATE TABLE "Vacation_History" (
-        "id" SERIAL PRIMARY KEY,
-        "confirmed" boolean NOT NULL,
-        "from" date NOT NULL,
-        "to" date NOT NULL,
-        "description" varchar(50)
-);
-
-
-CREATE TABLE "Work_History" (
-        "id" SERIAL PRIMARY KEY,
-        "id_company" SERIAL,
-        "id_vacation" SERIAL,
-        "from" date NOT NULL,
-        "to" date,
-        "salary" money NOT NULL,
-        "description" varchar(50),
-        FOREIGN KEY ("id_vacation") REFERENCES "Vacation_History"("id")
-);
-CREATE INDEX "Work_History_id_company"
-        ON "Work_History" ("id_company");
-
-
-CREATE TABLE "Group_History" (
-        "id" SERIAL PRIMARY KEY,
-        "id_work_history" SERIAL,
-        "id_vacation_history" SERIAL,
-        FOREIGN KEY ("id_vacation_history") REFERENCES "Vacation_History"("id"),
-        FOREIGN KEY ("id_work_history") REFERENCES "Work_History"("id")
-);
-
 CREATE TABLE "Adres" (
         "id" SERIAL PRIMARY KEY,
+        "id_adres" SERIAL,
         "country" varchar(20) NOT NULL,
         "city" varchar(30) NOT NULL,
         "street" varchar(40) NOT NULL,
         "house_number" varchar(10),
-        "zip_code" varchar(15) NOT NULL
+        "zip_code" varchar(15) NOT NULL,
+        "is_company" boolean
 );
+
+CREATE TABLE "Company" (
+        "id" SERIAL PRIMARY KEY,
+        "password" varchar(32) NOT NULL,
+        "name" varchar(50) NOT NULL,
+        "email" varchar(30),
+        "specialization" varchar(100) NOT NULL,
+        "description" varchar(500) NOT NULL,
+        "website" varchar(100),
+        "image" bytea
+);
+
+CREATE TABLE "Workstation" (
+        "id" SERIAL PRIMARY KEY,
+        "id_company" SERIAL,
+        "id_adres" SERIAL,
+        "name" varchar(30) NOT NULL,
+        "phone_number" varchar(20),
+        "email" varchar(30),
+        "limit" smallserial NOT NULL,
+        "description" varchar(50) NOT NULL,
+        FOREIGN KEY ("id_adres") REFERENCES "Adres"("id"),
+        FOREIGN KEY ("id_company") REFERENCES "Company"("id")
+);
+CREATE INDEX "Workstation_id_company"
+        ON "Workstation" ("id_company");
 
 CREATE TABLE "Employee" (
         "id" SERIAL PRIMARY KEY,
@@ -77,47 +67,18 @@ CREATE TABLE "Employee" (
         "birth" date NOT NULL,
         "phone_number" varchar(20) NOT NULL,
         "email" varchar(30) NOT NULL,
-        FOREIGN KEY ("id_adres") REFERENCES "Adres"("id"),
-        FOREIGN KEY ("id_history") REFERENCES "Vacation_History"("id")
-);
-
-CREATE TABLE "Workstation" (
-        "id" SERIAL PRIMARY KEY,
-        "id_company" SERIAL UNIQUE NOT NULL,
-        "id_adres" SERIAL,
-        "name" varchar(30) NOT NULL,
-        "phone_number" varchar(20),
-        "email" varchar(30),
-        "limit" smallserial NOT NULL,
-        "description" varchar(50) NOT NULL,
-        FOREIGN KEY ("id") REFERENCES "Employee"("id"),
-        FOREIGN KEY ("id_adres") REFERENCES "Adres"("id")
-);
-CREATE INDEX "Workstation_id_company"
-        ON "Workstation" ("id_company");
-
-
-CREATE TABLE "Company" (
-        "id" SERIAL PRIMARY KEY,
-        "id_adres" SERIAL,
-        "password" varchar(32) NOT NULL,
-        "name" varchar(50) NOT NULL,
-        "email" varchar(30),
-        "global_id" varchar(100) NOT NULL,       -- NIP or same
-        "specialization" varchar(100) NOT NULL,
-        "description" varchar(500) NOT NULL,
-        "website" varchar(100),
-        FOREIGN KEY ("id") REFERENCES "Workstation"("id_company"),
-        UNIQUE("id_adres"),
+        "image" bytea,
         FOREIGN KEY ("id_adres") REFERENCES "Adres"("id")
 );
 
 
 CREATE TABLE "Skill" (
         "id" SERIAL PRIMARY KEY,
+        "type" varchar(30) NOT NULL,
         "name" varchar(30) NOT NULL,
         "description" varchar(50)
 );
+
 
 CREATE TABLE "List_Skills" (
         "id" SERIAL PRIMARY KEY,
@@ -128,5 +89,27 @@ CREATE TABLE "List_Skills" (
         FOREIGN KEY ("id_list_skills") REFERENCES "Employee"("id"),
         FOREIGN KEY ("id_list_skills") REFERENCES "Workstation"("id")
 );
+
 CREATE INDEX "List_Skills_id_list_skills"
         ON "List_Skills" ("id_list_skills");
+
+
+
+CREATE TABLE "Work_History" (
+        "id" SERIAL PRIMARY KEY,
+        "id_company" SERIAL,
+        "from" date NOT NULL,
+        "to" date,
+        "description" varchar(50),
+        FOREIGN KEY ("id_company") REFERENCES "Company"("id"),
+        FOREIGN KEY ("id_company") REFERENCES "Employee"("id")
+);
+CREATE INDEX "Work_History_id_company"
+        ON "Work_History" ("id_company");
+
+
+
+
+
+
+
