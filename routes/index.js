b9470b4ba29d1f1b205d4835c36496edb5fb42ad
mmazepa <maksymilianmariusz@gmail.com>
2018-11-23 03:27:01 +0100

    Sortowanie niektórych tabel, wyciągania danych z bazy dalszy ciąg, poprawki wizualne.

--- routes/index.js
+++ routes/index.js
@@ -35,9 +35,21 @@ router.get('/', function(req, res, next)
 router.get('/firm', function(req,res)
 {
     session = req.session || session;
-    res.render('firm.ejs',
+    var firms = req.firms || firms;
+
+    db.any('SELECT * FROM "Company"', [true])
+    .then(function(data)
     {
-        session: session
+        firms = data;
+        res.render('firm.ejs',
+        {
+            session: session,
+            firms: firms
+        });
+    })
+    .catch(function(error)
+    {
+        console.log(error);
     });
 });
 
@@ -65,9 +77,27 @@ router.get('/firms', function(req,res)
 router.get('/worker', function(req,res)
 {
     session = req.session || session;
-    res.render('worker.ejs',
+    var employees = req.employees || employees;
+
+    db.any('SELECT e.first_name, e.last_name, e.birth, e.phone_number, e.email, ' +
+    'e.password, a.country, a.city, a.street, a.house_number, a.zip_code, ' +
+    'c.name AS company_name, c.specialization, wh.from, wh.to, wh.description ' +
+    'FROM "Work_History" AS "wh" INNER JOIN "Company" AS "c" ' +
+    'ON wh.id_company = c.id INNER JOIN "Employee" AS "e" ' +
+    'ON wh.id_emplyee = e.id INNER JOIN "Adres" AS a ' +
+    'ON e.id_adres = a.id WHERE e.last_name=\'Kowalski\'', [true])
+    .then(function(data)
     {
-        session: session
+        employees = data;
+        res.render('worker.ejs',
+        {
+            session: session,
+            employees: employees
+        });
+    })
+    .catch(function(error)
+    {
+        console.log(error);
     });
 });
 
@@ -99,7 +129,7 @@ router.get('/addresses', function(req,res)
     session = req.session || session;
     var addresses = req.addresses || addresses;
 
-    db.any('SELECT * FROM "Adres"', [true])
+    db.any('SELECT * FROM "Adres" ORDER BY country, city, street', [true])
     .then(function(data)
     {
         addresses = data;
@@ -121,8 +151,9 @@ router.get('/skills', function(req,res)
     var skills = req.skills || skills;
 
     db.any('SELECT e.first_name, e.last_name, s.type, s.name, s.description ' +
-    'FROM "List_Skills" AS ls INNER JOIN "Employee" AS e ON ls.id_owner = e.id ' +
-    'INNER JOIN "Skill" AS s ON ls.id_skill = s.id', [true])
+        'FROM "List_Skills" AS ls INNER JOIN "Employee" AS e ON ls.id_owner = e.id ' +
+        'INNER JOIN "Skill" AS s ON ls.id_skill = s.id ' +
+        'ORDER BY e.last_name, e.first_name, s.type, s.name', [true])
     .then(function(data)
     {
         skills = data;
