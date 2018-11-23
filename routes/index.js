492748c7e42acb28befc9a26cd0bbdb32a792dc2
mmazepa <maksymilianmariusz@gmail.com>
2018-11-23 02:41:26 +0100

    SELECT dla złączeń tabel + uzupełnienie reszty SELECT'ów o złączenia.

--- routes/index.js
+++ routes/index.js
@@ -76,7 +76,9 @@ router.get('/workers', function(req,res)
     session = req.session || session;
     var employees = req.employees || employees;
 
-    db.any('SELECT * FROM "Employee"', [true])
+    db.any('SELECT e.first_name, e.last_name, e.birth, e.phone_number, e.email, ' +
+            'a.country, a.city, a.street, a.house_number, a.zip_code ' +
+            'FROM "Employee" AS e INNER JOIN "Adres" AS a ON e.id_adres = a.id', [true])
     .then(function(data)
     {
         employees = data;
@@ -118,7 +120,9 @@ router.get('/skills', function(req,res)
     session = req.session || session;
     var skills = req.skills || skills;
 
-    db.any('SELECT * FROM "Skill"', [true])
+    db.any('SELECT e.first_name, e.last_name, s.type, s.name, s.description ' +
+    'FROM "List_Skills" AS ls INNER JOIN "Employee" AS e ON ls.id_owner = e.id ' +
+    'INNER JOIN "Skill" AS s ON ls.id_skill = s.id', [true])
     .then(function(data)
     {
         skills = data;
@@ -139,7 +143,11 @@ router.get('/workstations', function(req,res)
     session = req.session || session;
     var workstations = req.workstations || workstations;
 
-    db.any('SELECT * FROM "Workstation"', [true])
+    db.any('SELECT c.name AS company_name, w.name AS workstation_name, ' +
+            'w.phone_number, w.email, w.limit, w.description, a.country, ' +
+            'a.city, a.street, a.house_number, a.zip_code ' +
+            'FROM "Workstation" AS w INNER JOIN "Company" AS c ' +
+            'ON w.id_company = c.id INNER JOIN "Adres" AS a ON w.id_adres = a.id', [true])
     .then(function(data)
     {
         workstations = data;
@@ -147,6 +155,30 @@ router.get('/workstations', function(req,res)
         {
             session: session,
             workstations: workstations
+        });
+    })
+    .catch(function(error)
+    {
+        console.log(error);
+    });
+});
+
+router.get('/workHistory', function(req,res)
+{
+    session = req.session || session;
+    var workHistory = req.workHistory || workHistory;
+
+    db.any('SELECT c.name, e.first_name, e.last_name, wh.from, wh.to, wh.description ' +
+        'FROM "Work_History" AS "wh" INNER JOIN "Company" AS "c" ON wh.id_company = c.id ' +
+        'INNER JOIN "Employee" AS "e" ON wh.id_emplyee = e.id', [true])
+    .then(function(data)
+    {
+        console.log(data);
+        workHistory = data;
+        res.render('crud/workHistory.ejs',
+        {
+            session: session,
+            workHistory: workHistory
         });
     })
     .catch(function(error)
